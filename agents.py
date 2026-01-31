2e547f30a6ccbb923cfede378fe23ee9a0232ce1
Merleth Munoz <merlethmunozra@gmail.com>
2026-01-31 13:52:15 -0700

    m edited briefly agents.py to explain purpose + added config.toml

---
+++ agents.py
@@ -0,0 +1,16 @@
+'''
+Purpose of this file:
+
+This will be like a place were I might run important parts of the code 
+for an agent to follow. 
+
+Kind of like implementing some ideas that I saw from some of my other
+classmates.
+
+'''
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
